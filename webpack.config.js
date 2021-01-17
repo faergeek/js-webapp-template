ea726348f9029958edf284d5938554faf2394862
Sergey Slipchenko <faergeek@ya.ru>
2021-01-17 19:46:12 +0300

    Use webpack-plugin-serve instead of webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -6,14 +6,16 @@ const path = require('path');
 const { RunScriptWebpackPlugin } = require('run-script-webpack-plugin');
 const webpack = require('webpack');
 const nodeExternals = require('webpack-node-externals');
+const { WebpackPluginServe } = require('webpack-plugin-serve');
 
 const BUILD_ROOT = path.resolve('build');
+const PUBLIC_ROOT = path.resolve(BUILD_ROOT, 'public');
 
 function makeConfig({ dev, node }) {
   return {
     name: node ? 'node' : 'browser',
     mode: dev ? 'development' : 'production',
-    watch: dev && node,
+    watch: dev,
     devtool: dev && !node ? 'inline-source-map' : 'source-map',
     target: node ? 'node' : 'web',
     stats: dev ? 'none' : undefined,
@@ -25,7 +27,7 @@ function makeConfig({ dev, node }) {
             'dotenv-flow/config',
             './src/node',
           ]
-        : ['./src/browser']
+        : [dev && 'webpack-plugin-serve/client', './src/browser']
       ).filter(Boolean),
     },
     output: {
@@ -35,20 +37,9 @@ function makeConfig({ dev, node }) {
         ? path.relative(BUILD_ROOT, '[resource-path]')
         : undefined,
       filename: `[name]${dev || node ? '' : '.[contenthash]'}.js`,
-      path: node ? BUILD_ROOT : path.resolve(BUILD_ROOT, 'public'),
+      path: node ? BUILD_ROOT : PUBLIC_ROOT,
       publicPath: dev ? 'http://localhost:8081/' : '/',
     },
-    devServer:
-      dev && !node
-        ? {
-            contentBase: false,
-            headers: { 'Access-Control-Allow-Origin': '*' },
-            hot: true,
-            injectClient: true,
-            noInfo: true,
-            port: 8081,
-          }
-        : undefined,
     externals: node
       ? nodeExternals({ allowlist: [/^webpack\/hot/] })
       : undefined,
@@ -93,13 +84,25 @@ function makeConfig({ dev, node }) {
     },
     plugins: [
       dev && new FriendlyErrorsWebpackPlugin({ clearConsole: false }),
-      dev && new webpack.HotModuleReplacementPlugin(),
+      dev && node && new webpack.HotModuleReplacementPlugin(),
       dev &&
         node &&
         new RunScriptWebpackPlugin({
           name: 'main.js',
           nodeArgs: ['--inspect'],
           signal: true,
+        }),
+      dev &&
+        !node &&
+        new WebpackPluginServe({
+          client: { retry: true },
+          hmr: 'refresh-on-failure',
+          log: { level: 'error' },
+          port: 8081,
+          static: [PUBLIC_ROOT],
+          waitForBuild: true,
+          middleware: (app, builtins) =>
+            builtins.headers({ 'Access-Control-Allow-Origin': '*' }),
         }),
       !node &&
         new AssetsPlugin({
