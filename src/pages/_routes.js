84b5626e576e689ecc5568394716a9dc835baba2
Sergey Slipchenko <faergeek@ya.ru>
2021-01-21 23:49:54 +0300

    Add magic comments to dynamic imports

--- src/pages/_routes.js
+++ src/pages/_routes.js
@@ -4,19 +4,19 @@ export const routes = prepareRoutes([
   {
     name: 'home',
     path: '',
-    resolve: () => import('./home'),
+    resolve: () => import(/* webpackChunkName: "home" */ './home'),
     respond: ({ resolved }) => ({ body: resolved.default }),
   },
   {
     name: 'about',
     path: 'about',
-    resolve: () => import('./about'),
+    resolve: () => import(/* webpackChunkName: "about" */ './about'),
     respond: ({ resolved }) => ({ body: resolved.default }),
   },
   {
     name: '404',
     path: '(.*)',
-    resolve: () => import('./_404'),
+    resolve: () => import(/* webpackChunkName: "404" */ './_404'),
     respond: ({ resolved }) => ({
       meta: { status: 404 },
       body: resolved.default,
