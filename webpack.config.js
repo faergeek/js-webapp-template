98d394fffbbb6d5dfc2cf4fc4b858160e57c0ace
Sergey Slipchenko <faergeek@ya.ru>
2021-02-16 08:59:12 +0300

    Revert "webpack.config.js -> webpack.config.mjs"
    This reverts commit 77340b90c665837e6175e9b14a51481cbb252379.

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,16 @@
-import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
-import AssetsPlugin from 'assets-webpack-plugin';
-import CssMinimizerPlugin from 'css-minimizer-webpack-plugin';
-import FriendlyErrorsWebpackPlugin from 'friendly-errors-webpack-plugin';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import path from 'path';
-import { RunScriptWebpackPlugin } from 'run-script-webpack-plugin';
-import webpack from 'webpack';
-import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
-import nodeExternals from 'webpack-node-externals';
-import { WebpackPluginServe } from 'webpack-plugin-serve';
-import WebpackBar from 'webpackbar';
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const AssetsPlugin = require('assets-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+const { RunScriptWebpackPlugin } = require('run-script-webpack-plugin');
+const webpack = require('webpack');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const nodeExternals = require('webpack-node-externals');
+const { WebpackPluginServe } = require('webpack-plugin-serve');
+const WebpackBar = require('webpackbar');
 
-const SRC_ROOT = path.resolve('src');
 const BUILD_ROOT = path.resolve('build');
 const PUBLIC_ROOT = path.resolve(BUILD_ROOT, 'public');
 
@@ -57,7 +56,7 @@ function makeConfig({ dev, node, watch = false }) {
       rules: [
         {
           test: /\.js$/,
-          include: SRC_ROOT,
+          include: path.resolve('src'),
           loader: 'babel-loader',
           options: {
             caller: { watch },
@@ -181,7 +180,7 @@ function makeConfig({ dev, node, watch = false }) {
   };
 }
 
-export default (env, argv) => {
+module.exports = (env, argv) => {
   return argv.mode
     ? [
         makeConfig({
