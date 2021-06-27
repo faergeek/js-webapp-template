dda3c9a837d1f273c97b1ef1163e397c95b360b7
Sergey Slipchenko <me@faergeek.online>
2021-06-27 16:09:10 +0300

    Update @faergeek/make-webpack-config

--- src/nodeRequestHandler.tsx
+++ src/nodeRequestHandler.tsx
@@ -1,6 +1,6 @@
 import * as compression from 'compression';
 import * as express from 'express';
-import { readFileSync } from 'fs';
+import { readFile } from 'fs/promises';
 import * as path from 'path';
 import renderToString from 'preact-render-to-string';
 import invariant from 'tiny-invariant';
@@ -20,39 +20,40 @@ function getEntryUrls(entry: string | string[] | undefined) {
   return [];
 }
 
-const { main } = JSON.parse(
-  readFileSync(path.resolve(__dirname, 'webpack-assets.json'), 'utf-8')
-);
-
 export const app = express().use(
   compression(),
   express.static(path.resolve('public')),
   express.static(path.resolve('build', 'public'), {
-    maxAge: __DEV__ ? undefined : '1 year',
+    immutable: !import.meta.webpackHot,
+    maxAge: import.meta.webpackHot ? undefined : '1 year',
   }),
-  (_req, res) => {
-    res.set('Content-Type', 'text/html').send(
-      `<!doctype html>${renderToString(
-        <html lang="en">
-          <meta charSet="utf-8" />
-          <title>JS WebApp Template</title>
-          <meta name="viewport" content="width=device-width,initial-scale=1" />
-          {getEntryUrls(main.css).map(href => (
-            <link key={href} rel="stylesheet" href={href} />
-          ))}
-          {getEntryUrls(main.js).map(src => (
-            <script
-              key={src}
-              crossOrigin={
-                process.env.NODE_ENV === 'production' ? undefined : 'anonymous'
-              }
-              defer
-              src={src}
+  async (_req, res, next) => {
+    try {
+      const { main } = JSON.parse(
+        await readFile(path.resolve(__dirname, 'webpack-assets.json'), 'utf-8')
+      );
+
+      res.set('Content-Type', 'text/html').send(
+        `<!doctype html>${renderToString(
+          <html lang="en">
+            <meta charSet="utf-8" />
+            <title>JS WebApp Template</title>
+            <meta
+              name="viewport"
+              content="width=device-width,initial-scale=1"
             />
-          ))}
-          <App />
-        </html>
-      )}`
-    );
+            {getEntryUrls(main.css).map(href => (
+              <link key={href} rel="stylesheet" href={href} />
+            ))}
+            {getEntryUrls(main.js).map(src => (
+              <script key={src} defer src={src} />
+            ))}
+            <App />
+          </html>
+        )}`
+      );
+    } catch (err) {
+      next(err);
+    }
   }
 );
