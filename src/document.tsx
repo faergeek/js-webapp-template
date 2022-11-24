7290318ee6cb89e8697c82f3d853ee3abec9b445
Sergey Slipchenko <faergeek@gmail.com>
2022-11-24 07:45:19 +0400

    Use StrictMode as the top-most wrapper

--- src/document.tsx
+++ src/document.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react';
 import { ScrollRestoration } from 'react-router-dom';
 
 import { Meta } from './_core/meta';
@@ -9,45 +8,43 @@ export function Document({ children }: { children: React.ReactNode }) {
   const { css, js, nonce, routerState } = useEntryContext();
 
   return (
-    <StrictMode>
-      <html lang="en" suppressHydrationWarning>
-        <head suppressHydrationWarning>
-          <meta charSet="utf-8" />
-          <meta name="viewport" content="width=device-width,initial-scale=1" />
-          <meta name="theme-color" content="#639" />
-          <link rel="icon" href="/favicon.ico" sizes="any" />
-          <link rel="icon" href="/icon.svg" type="image/svg+xml" />
-          <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-          <link rel="manifest" href="/manifest.json" />
-          <Meta />
+    <html lang="en" suppressHydrationWarning>
+      <head suppressHydrationWarning>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta name="theme-color" content="#639" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <Meta />
 
-          {css.map(href => (
-            <link key={href} rel="stylesheet" href={href} />
-          ))}
+        {css.map(href => (
+          <link key={href} rel="stylesheet" href={href} />
+        ))}
 
-          {js.map(src => (
-            <script
-              key={src}
-              defer
-              nonce={nonce}
-              src={src}
-              suppressHydrationWarning
-            />
-          ))}
-        </head>
-
-        <body suppressHydrationWarning>
-          {children}
-
-          {typeof window !== 'undefined' && <ScrollRestoration />}
-
-          <BrowserGlobal
-            name="__ENTRY_CONTEXT__"
+        {js.map(src => (
+          <script
+            key={src}
+            defer
             nonce={nonce}
-            value={{ css, js, nonce, routerState }}
+            src={src}
+            suppressHydrationWarning
           />
-        </body>
-      </html>
-    </StrictMode>
+        ))}
+      </head>
+
+      <body suppressHydrationWarning>
+        {children}
+
+        {typeof window !== 'undefined' && <ScrollRestoration />}
+
+        <BrowserGlobal
+          name="__ENTRY_CONTEXT__"
+          nonce={nonce}
+          value={{ css, js, nonce, routerState }}
+        />
+      </body>
+    </html>
   );
 }
