76e8d43815a1b478dfe462c26c57f96d5f8ba580
Sergey Slipchenko <faergeek@gmail.com>
2024-02-04 11:22:38 +0400

    Get rid of BrowserRoot component

--- src/browser.tsx
+++ src/browser.tsx
@@ -1,16 +1,53 @@
 import './browser.css';
 
 import { StrictMode } from 'react';
-import { hydrateRoot } from 'react-dom/client';
+import { hydrateRoot, type Root } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import invariant from 'tiny-invariant';
 
-import { BrowserRoot } from './browserRoot';
+import { Entry } from './entry';
+import { routes } from './routes';
 
-hydrateRoot(
-  document,
-  <StrictMode>
-    <BrowserRoot />
-  </StrictMode>,
-);
+const { __ENTRY_CONTEXT__ } = window;
+invariant(__ENTRY_CONTEXT__);
+const { css, js, nonce, routerState } = __ENTRY_CONTEXT__;
+__webpack_nonce__ = nonce;
+
+let root: Root | undefined;
+
+(function render() {
+  const router = createBrowserRouter(routes, {
+    hydrationData: routerState,
+  });
+
+  if (import.meta.webpackHot) {
+    import.meta.webpackHot.accept(['./routes'], async () => {
+      router.dispose();
+      render();
+    });
+  }
+
+  const el = (
+    <StrictMode>
+      <Entry css={css} js={js} nonce={nonce} router={router}>
+        <RouterProvider router={router} />
+      </Entry>
+    </StrictMode>
+  );
+
+  if (root) {
+    root.render(el);
+
+    if (import.meta.webpackHot) {
+      router.navigate(router.state.location, {
+        replace: true,
+        preventScrollReset: true,
+      });
+    }
+  } else {
+    root = hydrateRoot(document, el);
+  }
+})();
 
 if (navigator.serviceWorker) {
   navigator.serviceWorker.register('/sw.js');
